44ae157181d17b0164288a23d89b505d855f9b25
ernazarovich007 <105929120+ernazarovich007@users.noreply.github.com>
2022-05-25 20:07:46 +0500

    yangi o'zgarishlar

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6lf$+rvsefu3n6=%iei2$@vv^@d%)ese6()nk(gy+*q-n@bud)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
